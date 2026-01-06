6fc3fe0f465abfbddc0afc5f5b0de029c9a52cf4
xiaojiahuo123 <71112890+xiaojiahuo123@users.noreply.github.com>
2026-01-06 22:07:01 +0800

    更改

---
+++ src/day_16/List的常用实现类/test1.java
@@ -0,0 +1,15 @@
+package day_16.List的常用实现类;
+
+import lombok.Data;
+import org.junit.Test;
+
+import java.util.ArrayList;
+@Data
+public class test1 {
+    @Test
+    public void test1(){
+        ArrayList list = new ArrayList();
+    }
+
+
+}
